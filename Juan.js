0fa6edb58ed9b40d4b0fde08cf339a6aae658225
JuanDBta <juandavid_diaz@hotmail.com>
2023-02-19 15:43:44 -0500

    Beatiful Days Code First challenge

--- Juan.js
+++ Juan.js
@@ -1,3 +1,28 @@
+// Challenge 1: Beatiful NUmbers
 
+function beautifulDays(i, j, k) {
+let arr=[];
+for(let a=i; a<=j;a++)
+{
+    arr.push(a);
+}
+let reverse =[];
+for(let a=0;a<arr.length;a++)
+{
+reverse[a] = Number(String(arr[a]).split('').reverse().join(''));
+}
 
-//hi Juan how are you
+let number=0;
+let beatiful=0;
+for(let a=0;a<arr.length;a++)
+{
+number=Math.abs(arr[a]-reverse[a])/k;
+if(Number.isInteger(number)===true)
+  {beatiful++;}  
+}
+return beatiful;
+}
+let result=0;
+result=beautifulDays(20,23,6);
+console.log(result);
+
